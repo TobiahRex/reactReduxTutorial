b34aaff864b0bdfe142d0036a97b8156bac4e67d
RexCorpDev <rexcorpfx@gmail.com>
2016-08-16 18:45:39 -0700

    added CourseList.js

--- src/Components/course/CoursesPage.js
+++ src/Components/course/CoursesPage.js
@@ -1,28 +1,12 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      course: { title: '' },
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
     this.courseRow = this.courseRow.bind(this);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({ course });
-  }
-
-  onClickSave(event) {
-    this.props.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -34,17 +18,6 @@ class CoursesPage extends React.Component {
       <div>
         <h1>Courses</h1>
         {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title} />
-        <input
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave} />
-
       </div>
     );
   }
@@ -52,7 +25,7 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
-  createCourse: PropTypes.func.isRequired,
+  actions: PropTypes.object.isRequired,
 };
 
 function mapStateToProps(state, ownProps) {
@@ -63,7 +36,8 @@ function mapStateToProps(state, ownProps) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    createCourse: course => dispatch(courseActions.createCourse(course)),
+    actions: bindActionCreators(courseActions, dispatch), // same as :
+    // createCourse: course => dispatch(courseActions.createCourse(course)),
   };
 }
 
