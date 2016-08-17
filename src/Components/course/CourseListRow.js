e74bce79e9a249276c69f257047a3db709dda605
RexCorpDev <rexcorpfx@gmail.com>
2016-08-16 20:24:47 -0700

    adding  Manage course page

--- src/Components/course/CourseListRow.js
+++ src/Components/course/CourseListRow.js
@@ -5,7 +5,7 @@ function CourseListRow({ course }) {
   return (
     <tr>
       <td><a href={course.watchHref}>Watch</a></td>
-      <td><a href={course.watchHref}>{course.title}</a></td>
+      <td><Link to={`/course/${course.id}`}>{course.title}</Link></td>
       <td>{course.authorId}</td>
       <td>{course.category}</td>
       <td>{course.length}</td>
